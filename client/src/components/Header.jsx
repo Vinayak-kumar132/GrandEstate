b0cb871626b4ebf609affd238fb56e08221d3d70
Vinayak-kumar132 <vinayakkumar8213@gmail.com>
2025-02-08 22:24:33 +0530

    Made protected route for profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa";
 import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux';
 
 
 export default function Header() {
+   
+  const {currentUser}=useSelector(state=>state.user);
+
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to="/">
         <h1 className='font-bold text-sm sm:text-xl flex flex-row'>
-          <span className='text-slate-500'>Vinayak</span>
+          <span className='text-slate-500'>Grand</span>
           <span className='text-slate-700'>Estate</span>
         </h1>
         </Link>
@@ -20,16 +25,20 @@ export default function Header() {
           />
           <FaSearch className='text-slate-600'/>
         </form>
-        <ul className='flex gap-4'>
+        <ul className='flex gap-4 items-center font-semibold '>
           <Link to="/">
           <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer select-none'>Home</li></Link>
           
           <Link to="about">
           <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer select-none'>About</li>
           </Link>
-
-          <Link to="sign-in">
-          <li className='text-slate-700 hover:underline cursor-pointer select-none'>Sign in</li>
+          
+          <Link to="/profile">
+          {
+            currentUser?(<img className='rounded-full h-9 w-9 object-cover border-2 border-gray-600 
+             p-[1px] bg-gray-200' src={currentUser.avatar} alt="profile"/>):
+            (<li className='text-slate-700 hover:underline cursor-pointer select-none'>Sign in</li>)
+          }
           </Link>
         </ul>
       </div>
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-hot-toast";
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
+import icon from '../asset/googleicn.svg'
 
 
 export default function OAuth() {
@@ -38,8 +39,10 @@ export default function OAuth() {
 
   return (
     // it is submit by default we have changed it to button
-    <button onClick={handleGoogleClick} type="button" className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-     Continue with google
+    <button onClick={handleGoogleClick} type="button" className='bg-blue-100 hover:bg-blue-200 border-2 border-blue-200 text-white py-2 px-3 transition-all duration-200 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 flex gap-6 items-center justify-center'>
+        
+         <img src={icon} alt="logo" loading='lazy' className='w-[30px]' />
+        <p className='text-black font-semibold'>Continue with google</p>
     </button>
   )
 }
