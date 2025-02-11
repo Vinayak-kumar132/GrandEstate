317d5a9af9528b7dc124a9129236294ba7a1e8fe
Vinayak-kumar132 <vinayakkumar8213@gmail.com>
2025-02-12 00:19:36 +0530

    Completed update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -32,8 +32,8 @@
 //       const fileName=new Date().getTime()+file.name;
 //       const storageRef=ref(storage,fileName);
 //       const uploadTask=uploadBytesResumable(storageRef,file);
-   
-     
+
+
 //       uploadTask.on(
 //         "state_changed",
 //         (snapshot) => { // snapshot is callback
@@ -48,7 +48,7 @@
 //         },
 
 //         () => {
-          
+
 //           //uploadTask.snapshot.ref is the actual reference pointing to this file.
 //           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
 //             setFormData({ ...formData, avatar: downloadURL })
@@ -56,8 +56,8 @@
 //           // yha formdata ke avatar field ko downloadURL se update kr rhe h 
 //         }
 //       );
-      
-      
+
+
 //   };
 //   const handleChange = (e) => {
 //     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -67,18 +67,18 @@
 //     e.preventDefault();
 //     console.log(formData);  // Ye backend ko send karne ke liye modify karo
 //   };
- 
+
 
 //   return (
 //     <div className='p-3 max-w-lg mx-auto'>
 //       <h1 className='text-3xl font-bold text-center my-7'>Profile</h1>
-      
+
 //       <form className='flex flex-col gap-4'onSubmit={handleSubmit}>
 //         <input
 //         onChange={(e)=>setFile(e.target.files[0])}
 //         type="file" ref={fileRef} hidden accept='image/*' />
 //          <img onClick={()=>fileRef.current.click()}  src={currentUser?.avatar || currentUser?.validUser?.avatar} alt="profile" className='rounded-3xl border-4 border-gray-700 h-24 w-24 p-[1px] cursor-pointer bg-white self-center mt-2' />
-        
+
 
 //          <p className="text-sm self-center">
 //           {fileUploadError ? (
@@ -101,7 +101,7 @@
 //           placeholder="Username"
 //           defaultValue={currentUser.username}
 //           onChange={handleChange}
-          
+
 //         />
 //         <input
 //           className="border p-3 rounded-lg outline-none"
@@ -110,14 +110,14 @@
 //           placeholder="Email"
 //           defaultValue={currentUser.email}
 //           onChange={handleChange}
-         
+
 //         />
 //         <input
 //           className="border p-3 rounded-lg outline-none"
 //           id="password"
 //           type="password"
 //           placeholder="Password"
-          
+
 //           />
 
 //           <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
@@ -138,16 +138,28 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { getStorage, ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-hot-toast";
 import { app } from '../firebase';
+import { updateUserStart, updateUserSuccess, updateUserfailure } from '../redux/user/userSlice'
+import { useDispatch } from 'react-redux';
+
+
+
+
+
+
+
 
 export default function Profile() {
-  const { currentUser } = useSelector(state => state.user);
+  const { currentUser,loading,error } = useSelector(state => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-  
+
+  const dispatch = useDispatch();
+
   const auth = getAuth();
 
   useEffect(() => {
@@ -204,19 +216,47 @@ export default function Profile() {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // prevent refreshing the page
+    // console.log(formData);
+    // console.log(currentUser);
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser?.validUser?._id || currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+
+      });
+      const data = await res.json();
+      console.log(" Updated user ka response..",data);
+      if (data.success === false) {
+        // toast.error("Couldn't Update the User");
+        toast.error(data.message);
+
+        dispatch(updateUserfailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+
+      toast.success("Profile Updated Successfull");
+      
+
+    } catch (error) {
+      dispatch(updateUserfailure(error.message));
+    }
   };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-bold text-center my-7'>Profile</h1>
-      
+
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
-        <img onClick={() => fileRef.current.click()} src={formData.avatar||currentUser?.avatar || currentUser?.validUser?.avatar} alt="profile" className='rounded-3xl border-4 border-gray-700 h-24 w-24 p-[1px] cursor-pointer bg-white self-center mt-2 object-cover' />
-        
+        <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser?.avatar || currentUser?.validUser?.avatar} alt="profile" className='rounded-3xl border-4 border-gray-700 h-24 w-24 p-[1px] cursor-pointer bg-white self-center mt-2 object-cover' />
+
         <p className="text-sm self-center">
           {fileUploadError ? (
             <span className="text-red-700">Error Image Upload(image must be less than 2MB)</span>
@@ -252,7 +292,7 @@ export default function Profile() {
           placeholder="Password"
         />
 
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...':'Update'}</button>
       </form>
 
       <div className='flex justify-between mt-5'>
