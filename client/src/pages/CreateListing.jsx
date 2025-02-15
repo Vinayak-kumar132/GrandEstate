fe71cd5ab86291dc8a9843161349a1975781e6e1
Vinayak-kumar132 <vinayakkumar8213@gmail.com>
2025-02-15 20:48:01 +0530

    Complete upload image functionality  in listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,126 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { toast } from "react-hot-toast";
+import { app } from '../firebase';
+import { getAuth,onAuthStateChanged } from "firebase/auth";
+import { getDownloadURL, getStorage, uploadBytesResumable, ref } from 'firebase/storage';
 
 export default function CreateListing() {
+
+    const [files, setFiles] = useState([]);
+    // console.log("ye rhi files...",files);
+    const [formData, setFormData] = useState({
+        imageUrls: [],
+
+    })
+    const { currentUser, loading, error } = useSelector(state => state.user);
+    const [uploading, setUploading] = useState(false);
+    const [imageUploadError, setImageUploadError] = useState(false);
+
+    console.log("This is formData..", formData);
+
+    
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser);
+            } else {
+                setUser(null);
+            }
+        });
+
+        return () => unsubscribe(); // Cleanup listener on unmount
+    }, []);
+
+    
+    if (!currentUser) {
+        toast.error("User not authenticated!! please signin");
+        return null; // or return <div>Please sign in</div>;
+    }
+    
+
+    const handleImageSubmit = (e) => {
+        
+        try {
+            if (files.length === 0) {
+                toast.error("Please select a file first!");
+                return;
+            }
+
+            if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+                setUploading(true);
+                setImageUploadError(false);
+
+                // we are uploading more than 1 images ,so we have more than 1 asynchronous behaviour,so we need to wait for all of them it will upload one by one  so it return more than one promise
+                const promises = [];
+                // All the download URLs is stored in the promises
+
+                for (let i = 0; i < files.length; i++) {
+                    promises.push(storeImage(files[i]));
+                }
+                Promise.all(promises).then((urls) => {
+                    // setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls) });
+                    setFormData(prev => ({ ...prev, imageUrls: [...prev.imageUrls, ...urls] }));
+
+                    setImageUploadError(false);
+                    setUploading(false);
+                    setFiles([]); 
+
+
+                });
+            } else {
+                setImageUploadError("You can only upload 6 images per listing");
+                setUploading(false);
+                toast.error("Image Limit exceeds!!")
+
+            }
+        } catch (error) {
+            setImageUploadError("Image Upload failed");
+            setUploading(false);
+            toast.error("Image Upload failed");
+        }
+    };
+
+    const storeImage = async (file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on(
+                "state_changed",
+
+                (snapshot) => {
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(progress);
+
+                },
+                // here we ignore the process of uploading 
+                (error) => {
+                    reject(error);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                    })
+                    // All the download URLs is stored in the promises
+                }
+            )
+        });
+    }
+
+    const handleRemoveImage = (index) => {
+        setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.filter((_, i) => i != index),
+        })
+    }
+
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='font-semibold text-3xl text-center my-7'>Create a Listing</h1>
@@ -86,44 +206,44 @@ export default function CreateListing() {
 
                         <div className=' flex items-center gap-2'>
                             <input type="number" id='bedrooms' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg outline-none text-center'
-                            onInput={(e) => {
-                                if (e.target.value > 10) e.target.value = 10;
-                              }} />
+                                onInput={(e) => {
+                                    if (e.target.value > 10) e.target.value = 10;
+                                }} />
                             <span>Beds</span>
 
                         </div>
 
                         <div className=' flex items-center gap-2'>
                             <input type="number" id='bathroom' min='1' max='10' required className='p-3 border border-gray-300 text-center rounded-lg outline-none'
-                            onInput={(e) => {
-                                if (e.target.value > 10) e.target.value = 10;
-                              }} />
+                                onInput={(e) => {
+                                    if (e.target.value > 10) e.target.value = 10;
+                                }} />
                             <span>Bathroom</span>
 
                         </div>
 
 
                         <div className=' flex items-center gap-2'>
-                            <input type="number" id='regularPrice'  min='1' max='1000'
-                            required
-                            className='p-3 border border-gray-300 text-center rounded-lg outline-none'
+                            <input type="number" id='regularPrice' min='1' max='1000'
+                                required
+                                className='p-3 border border-gray-300 text-center rounded-lg outline-none'
                             />
                             <div className='flex flex-col'>
-                            <span>Regular price</span>
-                            <span className='opacity-60 text-xs'>($/month)</span>
+                                <span>Regular price</span>
+                                <span className='opacity-60 text-xs'>($/month)</span>
                             </div>
-                            
+
 
                         </div>
 
                         <div className=' flex items-center gap-2'>
                             <input type="number" id='discountedPrice' min='1' max='1000'
-                            required
-                            className='p-3 border border-gray-300 text-center rounded-lg outline-none'
+                                required
+                                className='p-3 border border-gray-300 text-center rounded-lg outline-none'
                             />
                             <div className='flex flex-col'>
-                            <span>Discounted price</span>
-                            <span className='opacity-60 text-xs'>($/month)</span>
+                                <span>Discounted price</span>
+                                <span className='opacity-60 text-xs'>($/month)</span>
                             </div>
 
                         </div>
@@ -140,19 +260,40 @@ export default function CreateListing() {
 
 
                     <div className='flex gap-4'>
-                        <input className='p-3 border border-gray-300 rounded w-full ' type="file" id="images" accept="image/*" multiple />
+                        <input
+                            onChange={(e) => setFiles(e.target.files)}
+                            className='p-3 border border-gray-300 rounded w-full ' type="file" id="images" accept="image/*" multiple />
 
-                        <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                        <button disabled={uploading}
+                            type="button" // it mention because it is inside the form now by uploading the picture it will not subit form
+                            onClick={handleImageSubmit}
+                            className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>{uploading ? "Uploading..." : "upload"}</button>
+
 
                     </div>
+                    <p className='text-red-700 text-small'>{imageUploadError && imageUploadError}</p>
+
+                    {
+                        formData.imageUrls.length > 0 &&
+                        formData.imageUrls.map((url, index) => (
+                            <div key={url} className="flex justify-between p-3 border border-gray-400 items-center rounded-lg">
+                                <img  src={url} alt="listing image" className='w-20 h-20 object-contain rounded-lg' />
+
+                                <button
+                                    onClick={() => handleRemoveImage(index)}
+                                    type="button" className='p-3 text-red-700 uppercase font-semibold hover:opacity-75'>Delete</button>
+                            </div>
+                        ))
+                    }
+
 
                     <button
-                className='bg-gray-700 hover:opacity-95 text-white uppercase disabled:opacity-80 rounded-lg p-3 mt-2'
-                >Create listing</button>
-                   
+                        className='bg-gray-700 hover:opacity-95 text-white uppercase disabled:opacity-80 rounded-lg p-3 mt-2'
+                    >Create listing</button>
+
                 </div>
 
-                
+
 
 
             </form>
